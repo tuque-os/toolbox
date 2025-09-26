7e9840923b7b457e52b499c57398506b13c13054
Tryton Lux <code@trytonlux.ca>
2025-09-25 22:07:00 -0400

    feat: add image info to os-release

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,3 +7,10 @@ set -ouex pipefail
 
 # run scripts
 /ctx/build_files/packages.sh
+
+{
+  echo "IMAGE_NAME=Tuque OS"
+  echo "IMAGE_VARIANT=Toolbox"
+  echo "IMAGE_VERSION=\"${VERSION:-00.00000000}\""
+  echo "IMAGE_HOME_URL=https://github.com/tuque-os/toolbox"
+} >>/usr/lib/os-release
